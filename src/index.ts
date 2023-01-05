47569905d61d6e4e6b22d593f08e14cde6e03195
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-01-04 23:06:46 -0500

    Remove extra code

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,3 @@ const THING = "thing" as const;
 export type TestType = {
   [key: `${typeof THING}_${string}`]: any;
 };
-
-const example: TestType = {
-  thing_asdf: "hello",
-  // @ts-expect-error - should be invalid
-  invalid: 123,
-};
